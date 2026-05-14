37398ad75fb2b2912adc129b5a3d5a8d88d14b67
redoyjurepakhi <redoyjurepakhi@gmail.com>
2026-05-14 13:14:36 +0600

    Initial upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nanotubepro.hrlabapp',
+  appName: 'NanoTube Pro',
+  webDir: 'dist'
+};
+
+export default config;
