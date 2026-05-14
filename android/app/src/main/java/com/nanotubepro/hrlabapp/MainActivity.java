3c6f4d12d749226dad4aa5739b0bea4687c7cb99
redoyjurepakhi <redoyjurepakhi@gmail.com>
2026-05-14 17:45:41 +0600

    Enabled background playback

--- android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
+++ android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
@@ -1,5 +1,33 @@
 package com.nanotubepro.hrlabapp;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = this.bridge.getWebView();
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setDomStorageEnabled(true);
+        settings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        // Do NOT pause WebView audio/video
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
