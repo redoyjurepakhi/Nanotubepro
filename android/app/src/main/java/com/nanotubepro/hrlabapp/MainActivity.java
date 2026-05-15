cff43dd21cedc8002f7db4d67ec58bf18e59d56f
redoyjurepakhi <redoyjurepakhi@gmail.com>
2026-05-15 20:47:33 +0600

    Added PiP mode on Android back press

--- android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
+++ android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
@@ -2,20 +2,54 @@ package com.nanotubepro.hrlabapp;
 
 import android.app.PictureInPictureParams;
 import android.os.Build;
+import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
-    @Override
-    public void onUserLeaveHint() {
-        super.onUserLeaveHint();
+    private boolean isVideoPlaying() {
+
+        WebView webView = this.bridge.getWebView();
+
+        String js =
+            "(function() {" +
+            "const v = document.querySelector('video');" +
+            "return v && !v.paused;" +
+            "})()";
+
+        return true;
+    }
+
+    private void enterPiP() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
             PictureInPictureParams params =
                 new PictureInPictureParams.Builder().build();
 
             enterPictureInPictureMode(params);
+        }
+    }
+
+    @Override
+    public void onUserLeaveHint() {
+        super.onUserLeaveHint();
+
+        enterPiP();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (isVideoPlaying()) {
+
+            enterPiP();
+
+        } else {
+
+            super.onBackPressed();
+
         }
     }
 }
