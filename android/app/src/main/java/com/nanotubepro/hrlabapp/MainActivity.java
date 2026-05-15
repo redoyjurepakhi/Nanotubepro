def0298cdf723d61f8282865217c57c2e25d81f1
redoyjurepakhi <redoyjurepakhi@gmail.com>
2026-05-15 21:37:31 +0600

    Added Android immersive fullscreen mode

--- android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
+++ android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.nanotubepro.hrlabapp;
 import android.app.PictureInPictureParams;
 import android.os.Build;
 import android.webkit.WebView;
-
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -21,6 +23,33 @@ public class MainActivity extends BridgeActivity {
         return true;
     }
 
+private void enableImmersiveMode() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+            getWindow().getInsetsController().hide(
+                WindowInsets.Type.statusBars()
+                | WindowInsets.Type.navigationBars()
+            );
+
+            getWindow().getInsetsController().setSystemBarsBehavior(
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+
+        } else {
+
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+
+        }
+    }
+
     private void enterPiP() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -30,6 +59,13 @@ public class MainActivity extends BridgeActivity {
 
             enterPictureInPictureMode(params);
         }
+    }
+
+  @Override
+    protected void onResume() {
+        super.onResume();
+
+        enableImmersiveMode();
     }
 
     @Override
