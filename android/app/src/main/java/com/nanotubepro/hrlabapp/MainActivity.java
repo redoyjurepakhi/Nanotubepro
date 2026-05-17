8503ed0eb063d2bb10b825d0fadf79c377abfc19
redoyjurepakhi <redoyjurepakhi@gmail.com>
2026-05-18 03:06:45 +0600

    Fix missing Android Bundle import

--- android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
+++ android/app/src/main/java/com/nanotubepro/hrlabapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.nanotubepro.hrlabapp;
 import com.nanotubepro.hrlabapp.ImmersivePlugin;
 import android.app.PictureInPictureParams;
 import android.os.Build;
+import android.os.Bundle;
 import android.webkit.WebView;
 import android.view.View;
 import android.view.WindowInsets;
